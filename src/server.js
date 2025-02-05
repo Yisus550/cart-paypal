13ddd20b1bdd9091a74ce8960d7887f895c04861
Yisus550 <jesus.alfonso2402@gmail.com>
2025-02-05 09:15:52 -0700

    feat: add route handlers for authentication, cart, payment, and product management

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 
@@ -8,5 +12,10 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
+
+server.use("/auth", authRoutes);
+server.use("/cart", cartRoutes);
+server.use("/payment", paymentRoutes);
+server.use("/products", productRoutes);
 
 module.exports = server;
